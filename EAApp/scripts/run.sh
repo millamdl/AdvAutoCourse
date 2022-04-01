898d58c705f3b16b0869d93c93c92525941ff565
Dean Millam <78962335+millamdl@users.noreply.github.com>
2022-03-31 22:00:03 -0500

    Added docker execution via script files

---
+++ EAApp/scripts/run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+set -e
+set -x
+
+project="e2etest"
+
+cd "$(dirname "${0}")/.."
+
+export COMPOSE_HTTP_TIMEOUT=200
+
+docker-compose -p "$project" build
+
+docker-compose -p "$project" up -d ea_api ea_webapp db selenium-hub node-docker 
+docker-compose -p "$project" up --no-deps ea_test
+
+exit_code=$(docker inspect ea_test -f '{{ .State.ExitCode }}')
+
+if [ $exit_code -eq 0 ]; then
+	exit $exit_code
+else
+	echo "Test failed"
+fi
